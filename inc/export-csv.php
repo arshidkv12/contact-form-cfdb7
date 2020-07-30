f46f483177b346107d11d316faf9a117f1aaad16
Ivijan-Stefan Stipic <creativform@gmail.com>
2020-07-30 07:25:31 +0200

    Update export-csv.php
    * UTF-8 CSV Export Fix

--- inc/export-csv.php
+++ inc/export-csv.php
@@ -5,7 +5,7 @@
 
 if (!defined( 'ABSPATH')) exit;
 
-class Expoert_CSV{
+class Export_CSV{
 
     /**
      * Download csv file
@@ -18,14 +18,17 @@ class Expoert_CSV{
         header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
         header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
         header("Last-Modified: {$now} GMT");
-
+		
         // force download
+		header("Content-Description: File Transfer");
+		header("Content-Encoding: UTF-8");
+		header("Content-Type: text/csv; charset=UTF-8");
         header("Content-Type: application/force-download");
         header("Content-Type: application/octet-stream");
         header("Content-Type: application/download");
 
         // disposition / encoding on response body
-        header("Content-Disposition: attachment;filename={$filename}");
+		header("Content-Disposition: attachment;filename={$filename}");
         header("Content-Transfer-Encoding: binary");
 
     }
@@ -48,7 +51,8 @@ class Expoert_CSV{
             $tmp       = str_replace( $unwanted, '', $aKeys );
             $heading[] = ucfirst( $tmp );
         }
-        fputcsv( $df, $heading );
+        fputs( $df, $bom = ( chr(0xEF) . chr(0xBB) . chr(0xBF) ) ); // UTF-8 fix
+		fputcsv( $df, $heading );
 
         foreach ( $array['form_id'] as $line => $form_id ) {
             $line_values = array();
@@ -80,9 +84,11 @@ class Expoert_CSV{
             $heading_row = $cfdb->get_results("SELECT form_id, form_value, form_date FROM $table_name
                 WHERE form_post_id = '$fid' ORDER BY form_id DESC LIMIT 1",OBJECT);
 
-            $heading_row = reset( $heading_row );
-            $heading_row = unserialize( $heading_row->form_value );
-            $heading_key = array_keys( $heading_row );
+            $heading_row    = reset( $heading_row );
+            $heading_row    = unserialize( $heading_row->form_value );
+            $heading_key    = array_keys( $heading_row );
+            $rm_underscore  = apply_filters('cfdb7_remove_underscore_data', true); 
+
 
             $total_rows  = $cfdb->get_var("SELECT COUNT(*) FROM $table_name WHERE form_post_id = '$fid' "); 
             $per_query    = 1000;
@@ -110,7 +116,12 @@ class Expoert_CSV{
                     $cfdb7_dir_url          = $upload_dir['baseurl'].'/cfdb7_uploads';
 
                     foreach ($resultTmp as $key => $value):
+                        $matches = array();
+
                         if ( ! in_array( $key, $heading_key ) ) continue;
+                        if( $rm_underscore ) preg_match('/^_.*$/m', $key, $matches);
+                        if( ! empty($matches[0]) ) continue;
+
                         if (strpos($key, 'cfdb7_file') !== false ){
                             $data[$key][$i] = $cfdb7_dir_url.'/'.$value;
                             continue;
