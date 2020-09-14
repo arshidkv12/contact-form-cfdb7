905f7d2b689512abc9936b8161301a48d563eaeb
Arshid <arshidkv12@gmail.com>
2020-09-14 17:21:56 +0530

    Update contact-form-cfdb-7.php

--- contact-form-cfdb-7.php
+++ contact-form-cfdb-7.php
@@ -7,7 +7,7 @@ Author: Arshid
 Author URI: http://ciphercoin.com/
 Text Domain: contact-form-cfdb7
 Domain Path: /languages/
-Version: 1.2.4.11
+Version: 1.2.4.12
 */
 
 function cfdb7_create_table(){
