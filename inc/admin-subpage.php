5f5ee4ca5d5ca4c6bb89cd323a25ece4f118ad85
kuro roli <kuroridoplayer@gmail.com>
2023-02-03 00:29:33 +0800

    form_values should be cleared for each result

--- inc/admin-subpage.php
+++ inc/admin-subpage.php
@@ -242,6 +242,7 @@ class CFDB7_List_Table extends WP_List_Table
         foreach ( $results as $result ) {
 
             $form_value = unserialize( $result->form_value );
+            $form_values = [];
 
             $link  = "<b><a href=admin.php?page=cfdb7-list.php&fid=%s&ufid=%s>%s</a></b>";
             if(isset($form_value['cfdb7_status']) && ( $form_value['cfdb7_status'] === 'read' ) )
